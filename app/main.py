ea67f4ae7b60acc84d7de775d01eac5317bc94eb
vtyagi26 <vaibhavtyagi9981@gmail.com>
2026-04-25 21:08:12 +0530

    Updated None issue

--- app/main.py
+++ app/main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.predict import router as predict_router
+from app.services.model_service import load_models
 
 app = FastAPI(title="Stock Prediction API")
 
+
+@app.on_event("startup")
+def startup_event():
+    load_models()
+    print("Models loaded successfully.")
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # tighten later
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
